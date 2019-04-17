56e5915187f75f3a638e5c968f69629389fb7d4b
Alex Deckert <adeckert23@gmail.com>
2019-04-17 18:10:19 -0400

    Final Wednesday changes

--- src/radar1_class.py
+++ src/radar1_class.py
@@ -27,7 +27,5 @@ class Radar1(object):
 
     def plot(self, values, *args, **kw):
         angle = np.deg2rad(np.r_[self.angles, self.angles[0]])
-        print(len(angle))
         values = np.r_[values, values[0]]
-        print(len(values))
         self.ax.plot(angle, values, *args, **kw)
--- src/radar_chart.py
+++ src/radar_chart.py
@@ -5,7 +5,7 @@ from math import pi
 import funcs as f
 from importlib import reload
 import dataframe_cleaner
-reload(unidecode)
+reload(radar1_class)
 import radar1_class
 from radar1_class import Radar1
 import unidecode
@@ -17,11 +17,12 @@ cols2 = ['Player', 'Rating', 'Goals', 'Assists', 'SpG', 'Drb', 'KeyP','PS%',
 
 off_cats = ['Player', 'Rating', 'Goals', 'Assists', 'SpG', 'Drb', 'KeyP','PS%',
             'Crosses', 'Fouled', 'mis_cont','Tackles', 'Inter']
-
+cats = ['Player', 'Rating', 'Goals', 'Assists', 'SpG', 'Drb', 'KeyP','PS%',
+        'Fouled', 'mis_cont']
 #Defensive currently has 13, need to take out 1 of them
 center_def_cats = ['Player', 'Rating', 'AvgP','PS%', 'mis_cont', 'AerialsWon',
                     'Tackles', 'Inter', 'Fouls', 'Clear', 'Blocks']
-outside_def_cats = ['Player', 'Rating', 'AvgP', 'Assists', 'Drb','PS%', 'Crosses',
+outside_def_cats = ['Player', 'Rating', 'Assists', 'Drb','PS%', 'Crosses',
             'mis_cont', 'Tackles', 'Inter', 'Fouls', 'Clear', 'Blocks']
 
 #-------------------------------------------------------------------------------
@@ -36,56 +37,42 @@ Bundesliga, Prem, La_liga, Ligue_1, Serie_a = league_seperator(df)
 
 
 from funcs import accent_stripper
-Prem['Player'] = f.accent_stripper(Prem['Player'].values)
+df['Player'] = f.accent_stripper(df['Player'].values)
 #--------------------------------------
 #Sending in the Premier League to the scaler
 from funcs import DF_Scaler
-scaled_df = DF_Scaler(df)
+scaled_df = DF_Scaler(Prem)
 
 #--------------------------------------
-OBchart_prep = df[outside_def_cats]
+chart_prep = scaled_df[outside_def_cats]
 #--------------------------------------
-
-df[df['Player']=='Achraf Hakimi']
-# Prem.columns
-# Prem['Blocks'].describe()
+Prem[Prem['Player']=='Trent Alexander-Arnold']
 
 #Playmakers to radar plot
-Mendy = OBchart_prep.loc[2775].values
-Gaya = OBchart_prep.loc[1429].values
-Chilwell = OBchart_prep.loc[103].values
-Hakimi = OBchart_prep.loc[1919].values
-Henrichs = OBchart_prep.loc[2266].values
-Wan_Bissaka = OBchart_prep.loc[77].values
-
-
+Walker = chart_prep.loc[139].values
+Young = chart_prep.loc[177].values
+Tripp = chart_prep.loc[403].values
+TAE = chart_prep.loc[435].values
+Bellerin = chart_prep.loc[502].values
+AWB = chart_prep.loc[77].values
 #Figure
 fig.clear()
 
 fig = plt.figure(figsize=(10, 10))
 #Name to appear
-titles = ['Rating', 'AvgP', 'Assists', 'Drb','PS%', 'Crosses',
-            'mis_cont', 'Tackles', 'Inter', 'Fouls', 'Clear', 'Blocks']
+titles =['Rating', 'Assists', 'Drb','PS%', 'Crosses', 'mis_cont', 'Tackles',
+        'Inter', 'Fouls', 'Clear', 'Blocks']
 #Numerical labels to be displayed along each axis
-labels = [np.around(np.linspace(0,10,6),2), np.around(np.linspace(0,91,6),2),
-    np.around(np.linspace(0,11,6),2), np.around(np.linspace(0,3.2,6),2),
-    np.around(np.linspace(0,100,6),2), np.around(np.linspace(0,2.3,6),2),
-    np.around(np.linspace(0,8.8,6),2), np.around(np.linspace(0,4.3,6),2),
-    np.around(np.linspace(0,2.9,6),2), np.around(np.linspace(0,2.3,6),2),
-    np.around(np.linspace(0,11,6),2), np.around(np.linspace(0,1.6,6),2)]
+labels = [np.around(np.linspace(0,10,6),2), np.around(np.linspace(0,9,6),2),
+    np.around(np.linspace(0,3.2,6),2), np.around(np.linspace(0,100,6),2),
+    np.around(np.linspace(0,3,6),2), np.around(np.linspace(0,6,6),2),
+    np.around(np.linspace(0,5,6),2), np.around(np.linspace(0,3.2,6),2),
+    np.around(np.linspace(0,4,6),2),np.around(np.linspace(0,6,6),2), np.around(np.linspace(0,1.6,6),2)]
 
 radar = Radar1(fig, titles, labels)
-# radar.plot(Mendy[1:], '-', lw=3, color='r', alpha=0.4, label=Mendy[0])
-# radar.plot(Gaya[1:], '-', lw=3, color='#F0770A', alpha=0.4, label=Gaya[0])
-# radar.plot(Chilwell[1:], '-', lw=3, color='b', alpha=0.4, label=Chilwell[0])
-radar.plot(Hakimi[1:], '-', lw=3, color='#E9E405', alpha=0.4, label=Hakimi[0])
-radar.plot(Henrichs[1:], '-', lw=3, color='#0A1190', alpha=0.4, label=Henrichs[0])
-radar.plot(Wan_Bissaka[1:], '-', lw=3, color='k', alpha=0.4, label=Wan_Bissaka[0])
-
+radar.plot(Young[1:], '-', lw=5, color='r', alpha=0.4, label=Young[0])
+radar.plot(AWB[1:], '-', lw=5, color='#FF00FF', alpha=0.4, label=AWB[0])
 
 radar.ax.legend()
-fig.suptitle('RB replacements', fontsize=16)
-fig.savefig('RB_replacements.png')
-
-
-#-------------------------------------------------------------------------------
+fig.suptitle('Premier League Right Backs', fontsize=22)
+fig.savefig('Prem_RB_Replacement.png')
