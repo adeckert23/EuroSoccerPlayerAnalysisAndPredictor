a6734972931f3f69013b1b5f8853bf5515fc4007
Alex Deckert <adeckert23@gmail.com>
2019-04-21 12:44:19 -0400

    web blog changes

--- routes.py
+++ routes.py
@@ -5,3 +5,7 @@ app = Flask(__name__) #Flask initialize
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+# @app.route("/Messi-vs-Ronaldo")
+# def comparison():
+#     return render_template('Messi_vs_Ronaldo.html')
